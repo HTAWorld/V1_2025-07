b9bdfc5d9c66da28000fd0b428deb3c9bd5342e9
jithuth <tjithu1992@gmail.com>
2025-07-07 21:06:38 +0530

    Initail Usermanagement

---
+++ Migrations/20250707152831_UpgradeUserModelWithPasswordHashing.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace V1_2025_07.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradeUserModelWithPasswordHashing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppleId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FacebookId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsKycVerified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMobileVerified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "KycDocumentNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KycDocumentType",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KycDocumentUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "KycVerifiedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastLoginDevice",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastLoginIP",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferralCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferredBy",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserStatus",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FacebookId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GoogleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsKycVerified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsMobileVerified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "KycDocumentNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "KycDocumentType",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "KycDocumentUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "KycVerifiedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginDevice",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginIP",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ReferralCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ReferredBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserStatus",
+                table: "Users");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using V1_2025_07;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("monsterconnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Multiplayer Tournament API",
+        Version = "v1",
+        Description = "API documentation for the Multiplayer Tournament platform."
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Multiplayer Tournament API v1");
+        options.RoutePrefix = "swagger"; // Visit /swagger
+    });
 }
 
 app.UseHttpsRedirection();
